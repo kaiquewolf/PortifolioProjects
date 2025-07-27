6259f529a825b4590a3dd4d536fcce307dd6bada
Kaique Silva <devkaitech@outlook.com>
2025-07-27 11:58:40 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const handleclick = () => {
+  const body = document.querySelector("body");
+  body.classList.toggle("light");
+
+    if(body.classList.contains( "light")) {
+        document.querySelector('img').src = "./assets/avatar-light.png";
+    } else {  
+        document.querySelector('img').src = "./assets/avatar.png";
+    }
+      
+}
